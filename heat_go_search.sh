7c64f86b99e0425c2b639482555610cd225be8d0
JoshGilliganNZ <85854577+JoshGilliganNZ@users.noreply.github.com>
2025-12-18 09:49:27 +1300

    Update heat_go_search.sh

--- heat_go_search.sh
+++ heat_go_search.sh
@@ -1,6 +1,1 @@
-awk -F'\t' 'BEGIN{IGNORECASE=1}
-{
-  go=$0
-  if (go ~ /GO:0009408|GO:0034605|GO:0009266|GO:0006986|GO:0006457|GO:0061077|GO:0031072|GO:0051087|GO:0033554|GO:0034620/)
-    print
-}' emapper_geneBest.annotations.tsv  > by_GO_heat.tsv
+awk -F'\t' '$0 ~ /GO:0009408|GO:0034605|GO:0009266/' emapper_geneBest.annotations.tsv  > by_heat_only.tsv
